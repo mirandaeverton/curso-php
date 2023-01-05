f22ad4fe4324a1730affd77da594eeb0b15e1bde
mirandaeverton <evertonm@tecinf.local>
2023-01-05 14:26:13 +0000

    Array as a map and array challenges

---
+++ array/desafio_meses.php
@@ -0,0 +1,19 @@
+<div class="titulo">Desafio Meses</div>
+
+<?php
+$meses = array(
+    1 => "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+);
+
+print_r($meses[5]);
--- rotas/array.php
+++ rotas/array.php
@@ -5,5 +5,21 @@
             <a href="exercicio.php?dir=array&file=basico">
                 Conceitos Básicos Array
             </a>
+        </li>
+        <li>
+            <a href="exercicio.php?dir=array&file=mapa">
+                Mapa
+            </a>
+        </li>
+        <li>
+            <a href="exercicio.php?dir=array&file=desafio_index">
+                Desafio Index
+            </a>
+        </li>
+        <li>
+            <a href="exercicio.php?dir=array&file=desafio_meses">
+                Desafio Meses
+            </a>
+        </li>
     </ul>
 </div>
